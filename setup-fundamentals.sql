fc34bc3d86829e7e450f800d032b5a30d83c83d2
Asad Aijaz Arain <aijaz.asadi.junkmails@gmail.com>
2024-05-26 18:04:25 +0000

    Introductory Video - Commands

--- setup-fundamentals.sql
+++ setup-fundamentals.sql
@@ -1,1 +1,12 @@
-select * from raw.jaffle_shop.customers
+
+
+{% for i in range(10) %}
+
+    select {{ i }} as number {% if not loop.last %} union all {% endif %}
+
+{% endfor %}
+
+-- with source as (
+--     select * from {{ source('tpch', 'customer') }}
+-- )
+
